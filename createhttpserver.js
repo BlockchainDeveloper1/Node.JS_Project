f18891ecdd39464197367403413c7411948084cf
Sanjana <sanjanagoutam062@gmail.com>
2025-01-27 13:27:11 +0530

    Concept of modules in node.js

--- createhttpserver.js
+++ createhttpserver.js
@@ -1,8 +1,13 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req , resp)=>{
-    resp.write("<h1>This is working</h1>");
-    resp.write("<p>this is a pharagraph</p>");
+    //request processing --> req
+    //for writing response --> resp
+    resp.write("<h1>Create a file using js extension and run it </h1>");
+    resp.write("<p> This is how we can create a localhost server </p>");
+    resp.end("That's all");
 });
 
-server.listen(1212);
+server.listen(3000 , ()=>{console.log("Server is running on port 👉 3000 ")});
+
+
